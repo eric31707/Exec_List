a328c80898eff055fea291fc8bba6870085e1e81
eric31707 <eric31707@DESKTOP-95TUUP6>
2022-10-24 23:43:26 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> Products=new List<string>();
+			Products.Add("apples");
+			Products.Add("peaches");
+			Products.Add("guavas");
+			Products.Add("strawberries");
+			Products.Add("blueberries");
+		}
+	}
+}
